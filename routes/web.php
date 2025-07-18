0f8bfc363fb1c0292f974983bac140bfb1df39ff
akihide-hatano <a.h.19841202@gmail.com>
2025-07-18 18:44:21 +0900

    web.phpのルーティングを追加し、MedicationController、RecordController、TimingTagControllerのコントローラーを作成しました。

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RecordController;
+use App\Http\Controllers\MedicationController;
+use App\Http\Controllers\TimingTagController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,5 +19,12 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+//内服記録のリソース
+Route::resource('records', RecordController::class);
+// 薬のリソースルート
+Route::resource('medications', MedicationController::class);
+// 服用タイミングのリソースルート
+Route::resource('timing_tags', TimingTagController::class); // base_timeのCRUD用
 
 require __DIR__.'/auth.php';

5ff59f1c7091a662dba58d4af5848a55ab676ecc
akihide-hatano <a.h.19841202@gmail.com>
2025-07-23 10:30:44 +0900

    FcmTokenを修正。

--- app/Models/FcmToken.php
+++ app/Models/FcmToken.php
@@ -1,27 +1,26 @@
-    <?php
+<?php
+namespace App\Models;
 
-    namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo; // この行を追記してください
 
-    use Illuminate\Database\Eloquent\Factories\HasFactory;
-    use Illuminate\Database\Eloquent\Model;
-    use Illuminate\Database\Eloquent\Relations\BelongsTo; // この行を追記してください
+class FcmToken extends Model
+{
+    use HasFactory;
 
-    class FcmToken extends Model
+    // FcmTokenモデルが持つカラムのうち、一括割り当てを許可するもの
+    protected $fillable = [
+        'user_id',
+        'token',
+    ];
+
+    /**
+     * このFCMトークンが属するユーザーを取得します。
+     */
+    public function user(): BelongsTo
     {
-        use HasFactory;
-
-        // FcmTokenモデルが持つカラムのうち、一括割り当てを許可するもの
-        protected $fillable = [
-            'user_id',
-            'token',
-        ];
-
-        /**
-         * このFCMトークンが属するユーザーを取得します。
-         */
-        public function user(): BelongsTo
-        {
-            return $this->belongsTo(User::class);
-        }
+        return $this->belongsTo(User::class);
     }
+}
     

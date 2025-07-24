ec3756443411c98fd703e6009645df658e00a3fc
akihide-hatano <a.h.19841202@gmail.com>
2025-07-25 07:15:05 +0900

    dashboardにflashメッセージを表示するためのコードを追加しました。

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,6 +8,16 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+
+                {{-- フラッシュメッセージの表示 --}}
+                    @if (session('status'))
+                        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
+                            <strong class="font-bold">成功！</strong>
+                            <span class="block sm:inline">{{ session('status') }}</span>
+                        </div>
+                    @endif
+                {{--フラッシュメッセージの表示ここまで--}}
+
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
 

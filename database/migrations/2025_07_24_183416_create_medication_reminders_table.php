afc761031403ecb5ded9028042e97f881e409543
akihide-hatano <a.h.19841202@gmail.com>
2025-07-24 22:19:51 +0900

    2025_07_24_183416_create_medication_reminders_tableの修正

--- database/migrations/2025_07_24_183416_create_medication_reminders_table.php
+++ database/migrations/2025_07_24_183416_create_medication_reminders_table.php
@@ -6,15 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+
     public function up(): void
     {
         Schema::create('medication_reminders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // usersテーブルへの外部キー
-            $table->string('patient_name')->nullable(); // 患者名（誰の内服忘れかを示す）
-            $table->string('event_type'); // 例: 'forgotten_medication'
-            $table->text('message')->nullable(); // 通知メッセージの本文（任意）
-            $table->boolean('is_read')->default(false); // 既読/未読フラグ（任意）
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('record_id')->nullable()->constrained('records', 'record_id')->onDelete('cascade');
+            $table->string('patient_name')->nullable();
+            $table->string('event_type');
+            $table->text('message')->nullable();
+            $table->boolean('is_read')->default(false);
             $table->timestamps();
         });
     }

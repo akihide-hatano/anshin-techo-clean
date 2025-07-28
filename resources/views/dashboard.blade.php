9644d231d0df0da2809f2bd0b935a35280f9fe6e
akihide-hatano <a.h.19841202@gmail.com>
2025-07-28 22:50:32 +0900

    dashboardの変更致しました。

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
                     @if (session('status'))
                         <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
                             <strong class="font-bold">成功！</strong>
-                            <span class="block sm:inline">{{ session('status') }}</span>
+                             <span class="block sm:inline">{{ session('status') }}</span>
                         </div>
                     @endif
                 {{--フラッシュメッセージの表示ここまで--}}

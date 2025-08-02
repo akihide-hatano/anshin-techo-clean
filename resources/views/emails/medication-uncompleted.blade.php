6f3c986241b7c8ac8000305fb6b35d92a05ba432
akihide-hatano <a.h.19841202@gmail.com>
2025-08-02 21:30:01 +0900

    medication-uncompleted.blade.phpを作成

---
+++ resources/views/emails/medication-uncompleted.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>内服忘れ通知</title>
+</head>
+<body>
+    <h1>内服忘れのお知らせ</h1>
+    <p>{{ $user->name }}さんの内服記録が未完了です。</p>
+    <p>確認をお願いします。</p>
+    <br>
+    <p>---</p>
+    <p>このメールはシステムからの自動送信です。</p>
+</body>
+</html>

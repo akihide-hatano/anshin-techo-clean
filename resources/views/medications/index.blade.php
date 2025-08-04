e5a0d823515ca59bb42ca82cecdf9ed0da6181cc
akihide-hatano <a.h.19841202@gmail.com>
2025-08-04 21:14:28 +0900

    icons/home.blade.phpを修正。 medications/index.blade.phpのbuttonのlayoutを修正。

--- resources/views/medications/index.blade.php
+++ resources/views/medications/index.blade.php
@@ -1,19 +1,13 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('薬一覧') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <h1 class="text-2xl font-bold mb-6 text-center">登録されている薬一覧</h1>
 
-                    {{-- 新しい薬を追加ボタン --}}
                     <div class="flex justify-end mb-6">
-                        <a href="{{ route('medications.create') }}" class="inline-flex items-center px-4 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-500 focus:bg-green-500 active:bg-green-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                        <a href="{{ route('medications.create') }}" class="inline-flex items-center px-4 py-2 gap-1 bg-gray-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-500 focus:bg-gray-500 active:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                            <x-icons.document class="size-6 text-white" />
                             新しい薬を追加
                         </a>
                     </div>
@@ -73,7 +67,7 @@
 
                     <div class="mt-8 text-center">
                         <a href="{{ route('dashboard') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                            トップページに戻る
+                            <x-icons.home class="size-6 text-white" />トップページに戻る
                         </a>
                     </div>
                 </div>

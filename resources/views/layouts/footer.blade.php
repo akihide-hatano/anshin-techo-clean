c6f099bbbafd7454c342949c48ff8e52fecec512
akihide-hatano <a.h.19841202@gmail.com>
2025-08-04 16:46:58 +0900

    create.blade.phpとfotter.blade.phpを作成と修正しました。

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="bg-gray-800 shadow mt-12 py-8">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex flex-col sm:flex-row justify-center items-center text-center sm:text-left">
+            <div class="mb-4 sm:mb-0">
+                <p class="text-sm text-gray-200 ">&copy; {{ date('Y') }} あんしん手帳. All rights reserved.</p>
+            </div>
+        </div>
+    </div>
+</footer>

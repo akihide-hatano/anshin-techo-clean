12d3d6ccdca6b092ecc43c9ad3ced3fd55158526
akihide-hatano <a.h.19841202@gmail.com>
2025-08-05 18:04:04 +0900

    records/index,createの修正をしました。

--- resources/views/records/index.blade.php
+++ resources/views/records/index.blade.php
@@ -1,11 +1,4 @@
 <x-app-layout>
-    {{-- ヘッダーのスロット（ページのタイトル） --}}
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('内服記録一覧') }}
-        </h2>
-    </x-slot>
-
     {{-- メインコンテンツ --}}
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">

a9402316d28930ebbf1a8aaf90328c2b6e84eaa0
akihide-hatano <a.h.19841202@gmail.com>
2025-08-08 22:47:07 +0900

    register.blade.phpにnotificationを追加しました。

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,6 +39,12 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        <div class="mt-4">
+            <x-input-label for="notification_email" :value="__('Notification Email')" />
+            <x-text-input id="notification_email" class="block mt-1 w-full" type="email" name="notification_email" :value="old('notification_email')" autocomplete="notification_email" />
+            <x-input-error :messages="$errors->get('notification_email')" class="mt-2" />
+        </div>
+
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
